4d08de55f597093580bb757fb203d41aa8307678
Artemka0-7 <pavlovskiy.07@inbox.ru>
2022-06-26 21:40:12 +0300

    next

---
+++ client/pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home;
